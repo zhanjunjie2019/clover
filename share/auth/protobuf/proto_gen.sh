ae977a3a1def4a347c14d86bde04789920adc720
詹俊杰 <17610656176@163.com>
2022-11-22 16:57:13 +0800

    feat:新增example模块，未完成

--- share/auth/protobuf/proto_gen.sh
+++ share/auth/protobuf/proto_gen.sh
@@ -3,10 +3,10 @@
 protoc --gofast_out=. *.proto
 
 function pause(){
-        read -n 1 -p "$*" INP
-        if [ $INP != '' ] ; then
-                echo -ne '\b \n'
-        fi
+  read -n 1 -p "$*" INP
+  if [ $INP != '' ] ; then
+          echo -ne '\b \n'
+  fi
 }
 
 pause 'Press any key to continue...'
--- starter-example/swag_init.sh
+++ starter-example/swag_init.sh
@@ -3,10 +3,10 @@
 swag init --pd
 
 function pause(){
-        read -n 1 -p "$*" INP
-        if [ $INP != '' ] ; then
-                echo -ne '\b \n'
-        fi
+  read -n 1 -p "$*" INP
+  if [ $INP != '' ] ; then
+          echo -ne '\b \n'
+  fi
 }
 
 pause 'Press any key to continue...'
